7ebbfb6e1c23f2ee342a2581148023bc0c27c9ef
toaignika <eugene.my88@gmail.com>
2022-03-28 17:49:01 +0300

    Added simple logic for tictactoe

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -7,13 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicTacToeComponent implements OnInit {
   board_layout = [
-    ['1', '2', '3'],
-    ['4', '5', '6'],
-    ['7', '8', '9']
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
   ]
+
+  turn = 'x'
+  turn_number = 1
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  makeMove(i:number, j:number) {
+    this.board_layout[i][j]=this.turn
+    if (this.checkWin(i,j) || this.turn_number == 9) {
+      alert('game over!')
+      // game over! this.turn wins
+      this.newGame()
+    } else {
+      this.turn = this.turn == 'x' ? 'o' : 'x'
+      this.turn_number++
+    }
+  }
+
+  checkWin(i:number, j:number) {
+    var row_result = this.board_layout[i].every( (val, i, arr) => val === this.turn )
+    var col = this.board_layout.map(function(value,index) { return value[j]; });
+    var col_result = col.every( (val, i, arr) => val === this.turn)
+
+    if (row_result || col_result) {
+      // console.log('true')
+      return true
+    } else {
+      // console.log('false')
+      return false
+    }
+  }
+
+
+  newGame() {
+    this.turn = 'x'
+    this.turn_number = 1
+    this.board_layout = [
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+      [' ', ' ', ' ']
+    ]
   }
 
 }
