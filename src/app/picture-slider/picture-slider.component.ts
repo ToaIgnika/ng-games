b7c6e816b5268b5ce5e2db059d4b91700fac4291
toaignika <eugene.my88@gmail.com>
2022-04-06 23:59:10 +0300

    Finished picture slider

--- src/app/picture-slider/picture-slider.component.ts
+++ src/app/picture-slider/picture-slider.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ResultDialogComponent } from './dialogs/result-dialog/result-dialog.component';
 
 @Component({
   selector: 'app-picture-slider',
@@ -6,10 +8,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./picture-slider.component.scss']
 })
 export class PictureSliderComponent implements OnInit {
+  imgIndex = [1,2,3,4,5,6,7,8,9,10,11,0]
+  imgIndexX = [
+    1,2,3,4,
+    5,6,7,8,
+    9,10,11,0]
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.shuffle()
+  }
+
+  moveSquare(i:number) {
+    if (i-1 >= 0 && this.imgIndexX[i-1] == 0) {
+      this.swap(i, i-1)
+    } else if (i+1 <= 11 && this.imgIndexX[i+1] == 0) {
+      this.swap(i, i+1)
+    } else if (i+4 <= 11 &&this.imgIndexX[i+4] == 0) {
+      this.swap(i, i+4)
+    } else if (i-4 >= 0 && this.imgIndexX[i-4] == 0) {
+      this.swap(i, i-4)
+    } else {
+    }
+    if (this.isSorted(this.imgIndexX)) {
+      this.showResult();
+    }
+  }
+
+  swap(i:number,j:number) {
+    let temp = this.imgIndexX[i];
+      this.imgIndexX[i] = 0;
+      this.imgIndexX[j] = temp;
+  }
+
+  isSorted(arr:number[]) {
+    for(let i = 0; i < arr.length-2; i++) {
+      if (arr[i+1]-arr[i] != 1) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  shuffle() {
+    let temp = JSON.parse(JSON.stringify(this.imgIndex))
+    let newId = []
+    while (temp.length > 0) {
+      let id = Math.floor(Math.random()*(temp.length-1))
+      newId.push(temp[id])
+      temp.splice(id, 1)
+    }
+    this.imgIndexX = newId
+  }
+
+  showResult() {
+    this.dialog.open(ResultDialogComponent, { width: '450px'}).afterClosed().subscribe(result => {
+      this.shuffle()
+    })
   }
 
 }
--- src/app/picture-slider/picture-slider.module.ts
+++ src/app/picture-slider/picture-slider.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { PictureSliderRoutingModule } from './picture-slider-routing.module';
 import { PictureSliderComponent } from './picture-slider.component';
+import { MaterialModule } from '../material/material.module';
+import { ResultDialogComponent } from './dialogs/result-dialog/result-dialog.component';
 
 
 @NgModule({
   declarations: [
-    PictureSliderComponent
+    PictureSliderComponent,
+    ResultDialogComponent
   ],
   imports: [
     CommonModule,
-    PictureSliderRoutingModule
+    PictureSliderRoutingModule,
+    MaterialModule
   ]
 })
 export class PictureSliderModule { }
