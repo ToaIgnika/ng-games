785a3674dde7f84af86774ac7e6874c68071e038
toaignika <eugene.my88@gmail.com>
2022-04-09 21:01:33 +0300

    Added card assets and deck generating functionality

--- src/app/blackjack/blackjack.component.ts
+++ src/app/blackjack/blackjack.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlackjackComponent implements OnInit {
 
+  deck_suit = [
+    'clubs',
+    'diamonds',
+    'hearts',
+    'spades',
+  ]
+  deck_val = [
+    'ace',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'jack',
+    'queen',
+    'king',
+  ]
+  deck: String[] = []
+
   constructor() { }
 
   ngOnInit(): void {
+    this.buildDeck();
   }
 
+  buildDeck() {
+    this.deck = []
+    this.deck_suit.forEach(suit => {
+      this.deck_val.forEach(val => {
+        this.deck.push(`${val}_of_${suit}`)
+      })
+    });
+    console.log(this.deck)
+  }
 }
